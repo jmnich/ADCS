973e6cf0883c8cca369b6a7db785273c76d7f854
Jakub Mnich <jk.mbot@gmail.com>
2016-10-15 22:22:42 +0200

    I2C interface finished

--- ADCS_V1/src/ApplicationControl.java
+++ ADCS_V1/src/ApplicationControl.java
@@ -1,5 +1,49 @@
+import FFDENetwork.FFDEEvent;
+import FFDENetwork.FFDEKernel;
+import FFDENetwork.FFDEObserver;
+import FFDENetwork.FFDEServer;
+
 /**
  * Created by Jakub on 10.10.2016.
+ *
+ * High level coordination and data handling.
  */
-public class ApplicationControl {
+public class ApplicationControl implements FFDEObserver, Runnable{
+
+    private HardwareCommunicationManager hardCom;
+    private FFDEServer ffdeServer;
+    private FFDEKernel ffdeKernel;
+
+    public ApplicationControl() {
+
+        ffdeServer = new FFDEServer("ADCS", 6666, this);
+
+
+        hardCom = new HardwareCommunicationManager();
+
+
+        ffdeServer.publish("rawAcc");
+        ffdeServer.publish("rawGyro");
+        ffdeServer.publish("rawMagnet");
+        ffdeServer.publish("rawBar");
+
+        ffdeServer.publish("filteredAngle");
+        ffdeServer.publish("filteredAngularVel");
+
+        Thread th = new Thread(this);
+        th.start();
+    }
+
+    @Override
+    public void notifyFFDE(FFDEEvent event) {
+
+    }
+
+    @Override
+    public void run() {
+        ffdeServer.waitUntilNetworkIsReady();
+
+
+
+    }
 }
